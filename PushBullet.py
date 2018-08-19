a44be964d4969b70fdddd03bda1f2cd4efeb86bb
rmit-s3635887-pavan-w <37763559+s3635887@users.noreply.github.com>
2018-08-19 18:59:20 +1000

    messages with corrected temperature

--- PushBullet.py
+++ PushBullet.py
@@ -6,6 +6,23 @@ from sense_hat import SenseHat
 
 ACCESS_TOKEN="o.QXA7Z8fuI1nfW793pN45mH9P9Np22Csg"
 
+# get CPU temperature
+def get_cpu_temp():
+  res = os.popen("vcgencmd measure_temp").readline()
+  t = float(res.replace("temp=","").replace("'C\n",""))
+  return(t)
+
+# use moving average to smooth readings
+def get_smooth(x):
+  if not hasattr(get_smooth, "t"):
+    get_smooth.t = [x,x,x]
+  get_smooth.t[2] = get_smooth.t[1]
+  get_smooth.t[1] = get_smooth.t[0]
+  get_smooth.t[0] = x
+  xs = (get_smooth.t[0]+get_smooth.t[1]+get_smooth.t[2])/3
+  return(xs)
+
+
 def send_notification_via_pushbullet(title, body):
     """ Sending notification via pushbullet.
         Args:
@@ -25,8 +42,13 @@ def send_notification_via_pushbullet(title, body):
 #main function
 def main():
     sense = SenseHat()
-    temp = sense.get_temperature()
-    temp = round(temp,1)
+    temp1 = sense.get_temperature_from_humidity()
+    temp2 = sense.get_temperature_from_pressure()
+    t_cpu = get_cpu_temp()
+    t = (t1+t2)/2
+    t_corr = t - ((t_cpu-t)/1.5)
+    t_corr = get_smooth(t_corr)
+    temp = round(t_corr,1)
     if temp < 23.0:
         temp = str(temp)
         ip_address = os.popen('hostname -I').read()
